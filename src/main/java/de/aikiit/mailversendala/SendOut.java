3da886eaebb99a7b96019cf36497f307e7785794
Hugo Hirsch <github@hugo-hirsch.de>
2017-05-16 23:07:20 +0200

    Issue #3: Add html mail version.

--- src/main/java/de/aikiit/mailversendala/SendOut.java
+++ src/main/java/de/aikiit/mailversendala/SendOut.java
@@ -1,11 +1,10 @@
 package de.aikiit.mailversendala;
 
 import com.google.common.base.Charsets;
-import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
+import org.apache.commons.mail.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
 
 /**
@@ -14,17 +13,29 @@ import java.util.Date;
 public class SendOut {
 
     private Email email;
+    private HtmlEmail htmlEmail;
 
     public SendOut() {
+        DefaultAuthenticator authenticator = new DefaultAuthenticator(MailConfig.USERNAME, MailConfig.PASSWORD);
+
         this.email = new SimpleEmail();
         email.setHostName(MailConfig.SMTP_HOST);
         email.setSmtpPort(MailConfig.SMTP_PORT);
-        email.setAuthenticator(new DefaultAuthenticator(MailConfig.USERNAME, MailConfig.PASSWORD));
+        email.setAuthenticator(authenticator);
         email.setSSLOnConnect(true);
+        email.setBounceAddress(MailConfig.FROM);
+
+        this.htmlEmail = new HtmlEmail();
+        htmlEmail.setHostName(MailConfig.SMTP_HOST);
+        htmlEmail.setSSLOnConnect(true);
+        htmlEmail.setBounceAddress(MailConfig.FROM);
+        htmlEmail.setAuthenticator(authenticator);
     }
 
     public static void main(String... args) throws Exception {
-        new SendOut().send(false);
+        SendOut sendOut = new SendOut();
+        sendOut.send(false);
+        sendOut.sendComplex(false);
     }
 
     public void send(boolean isTest) throws EmailException {
@@ -35,6 +46,25 @@ public class SendOut {
 
         email.setSubject(MailConfig.SUBJECT + new Date());
         email.setMsg("This is a test mail from Mailversendala... :-)");
+        if (!isTest) {
+            email.send();
+        }
+    }
+
+    public void sendComplex(boolean isTest) throws EmailException, MalformedURLException {
+        htmlEmail.addTo(MailConfig.TO, "John Doe Recipiento");
+        htmlEmail.setFrom(MailConfig.FROM, "Me");
+
+        // embed the image and get the content id
+        URL url = new URL("http://www.apache.org/images/asf_logo_wide.gif");
+        String cid = htmlEmail.embed(url, "Apache logo");
+
+        // set the html message
+        htmlEmail.setHtmlMsg("<html>The apache logo - <img src=\"cid:"+cid+"\"></html>");
+
+        // set the alternative message
+        htmlEmail.setTextMsg("Your email client does not support HTML messages - thus no Apache logo");
+
         if (!isTest) {
             email.send();
         }
