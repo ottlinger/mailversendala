cc79f820af9950ba17eabb353c1514b8c861d4cf
Hugo Hirsch <github@hugo-hirsch.de>
2017-05-24 22:10:32 +0200

    Issue #4: Basic architecture

--- src/main/java/de/aikiit/mailversendala/csv/CsvParser.java
+++ src/main/java/de/aikiit/mailversendala/csv/CsvParser.java
@@ -1,7 +1,28 @@
-package de.aikiit.mailversendala;
+package de.aikiit.mailversendala.csv;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
 
 /**
  * Created by hirsch on 24.05.17.
  */
 public class CsvParser {
+    private static final Logger LOG =
+            LogManager.getLogger(CsvParser.class);
+
+    public List<Mailing> parse(String language) {
+        List<Mailing> results = Lists.newArrayList();
+
+        if (Strings.isNullOrEmpty(language)) {
+            LOG.info("Will parse for all languages, which may mean more mails being sent out.");
+        }
+
+        return results;
+    }
+
+
 }
--- src/main/java/de/aikiit/mailversendala/csv/Mailing.java
+++ src/main/java/de/aikiit/mailversendala/csv/Mailing.java
@@ -1,7 +1,14 @@
 package de.aikiit.mailversendala.csv;
 
+import lombok.Builder;
+import lombok.Data;
+
 /**
  * Created by hirsch on 24.05.17.
  */
+@Builder
+@Data
 public class Mailing {
+    private String name;
+    private String email;
 }
