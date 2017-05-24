bc60cd45d315972e80d8e47956af18743d5e6642
Hugo Hirsch <github@hugo-hirsch.de>
2017-05-24 22:05:30 +0200

    Issue #4: Prepare CSV parsing.

---
+++ src/main/java/de/aikiit/mailversendala/csv/CsvParser.java
@@ -0,0 +1,7 @@
+package de.aikiit.mailversendala;
+
+/**
+ * Created by hirsch on 24.05.17.
+ */
+public class CsvParser {
+}
---
+++ src/main/java/de/aikiit/mailversendala/csv/Mailing.java
@@ -0,0 +1,7 @@
+package de.aikiit.mailversendala.csv;
+
+/**
+ * Created by hirsch on 24.05.17.
+ */
+public class Mailing {
+}
