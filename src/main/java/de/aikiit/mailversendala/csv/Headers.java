cc53a317614116afea04d9ff332a6fa445c144c6
Hugo Hirsch <github@hugo-hirsch.de>
2017-05-29 22:18:15 +0200

    Issue #4: add header constants

---
+++ src/main/java/de/aikiit/mailversendala/csv/Headers.java
@@ -0,0 +1,11 @@
+package de.aikiit.mailversendala.csv;
+
+/**
+ * Contains the headers that need to be in the CSV-file.
+ */
+public interface Headers {
+    String FIRSTNAME = "firstname";
+    String SURNAME = "surname";
+    String EMAIL = "email";
+    String LANGUAGE = "language";
+}
