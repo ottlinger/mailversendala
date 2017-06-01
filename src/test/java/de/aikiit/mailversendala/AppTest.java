191208841cea6940201ec6d35dad3832b8cd5c10
Hugo Hirsch <github@hugo-hirsch.de>
2017-06-02 00:21:36 +0200

    Issue #11: Raise test coverage due to demomode - no harm will be done

--- src/test/java/de/aikiit/mailversendala/AppTest.java
+++ src/test/java/de/aikiit/mailversendala/AppTest.java
@@ -6,15 +6,12 @@ import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
     @Test
     public void appIsNotNullAndLaunchable() throws IOException {
         assertThat(new App()).isNotNull();
-        // TODO App.main(null);
+        App.main(null);
     }
 
 }
