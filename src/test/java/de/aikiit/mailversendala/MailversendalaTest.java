f9db99130c38ce301c0eb394f66cd3c489ac40dc
Hugo Hirsch <github@hugo-hirsch.de>
2017-06-06 20:23:30 +0200

    Issue #11: Test extracted business logic

--- src/test/java/de/aikiit/mailversendala/MailversendalaTest.java
+++ src/test/java/de/aikiit/mailversendala/MailversendalaTest.java
@@ -6,21 +6,29 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by hirsch on 04.06.17.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class MailversendalaTest {
 
     private static final String CSV_NAME = "MailversendalaTest";
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Mock
+    private MailConfig configuration;
 
     @Before
     public void flushCSVData() throws IOException {
@@ -29,8 +37,14 @@ public class MailversendalaTest {
     }
 
     @Test
-    public void parseCSVFromARealFolder() {
+    public void parseCSVFromARealFolder() throws IOException {
+
+        when(configuration.getCsvPath()).thenReturn(testFolder.toString() + "/"+CSV_NAME);
+
+        Mailversendala.sendOut(configuration);
         assertThat(true).isTrue();
+
+
     }
 
 
