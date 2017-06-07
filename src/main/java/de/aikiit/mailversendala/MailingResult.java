b71294501c53cf5f4087796ce7b47c4d10dff668
Hugo Hirsch <github@hugo-hirsch.de>
2017-06-07 22:58:19 +0200

    Issue #12: Add test

--- src/main/java/de/aikiit/mailversendala/MailingResult.java
+++ src/main/java/de/aikiit/mailversendala/MailingResult.java
@@ -1,8 +1,27 @@
 package de.aikiit.mailversendala;
 
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by hirsch on 06.06.17.
  */
 public class MailingResult {
 
+    private AtomicInteger errorCounter;
+    private AtomicInteger mailCounter;
+
+    public void addError() {
+        if(errorCounter == null) {
+            this.errorCounter = new AtomicInteger(0);
+        }
+    }
+
+    public Optional<AtomicInteger> getErrorCounter() {
+        return Optional.ofNullable(errorCounter);
+    }
+
+    public Optional<AtomicInteger> getMailCounter() {
+        return Optional.ofNullable(mailCounter);
+    }
 }
