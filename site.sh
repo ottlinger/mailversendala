5af3d7989845b809c6be0746e533cdb36456cd72
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-28 11:30:32 +0100

    #304: add site infra

---
+++ site.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Generating new mvn site ..."
+./mvnw clean org.pitest:pitest-maven:mutationCoverage site:site 
+echo "DONE - ready to commit and push"
+cp -rf target/site/* docs
+git add -f docs/
+git commit -a -m "Issue #304: Update site via script"
+git push
