db2b7f5c336f892654924ab4c3e2ceaa88179bcc
Hugo Hirsch <github@hugo-hirsch.de>
2024-01-09 23:29:03 +0100

    Issue #304: Update site via script

---
+++ docs/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.aikiit.mailversendala"},{"l":"de.aikiit.mailversendala.csv"},{"l":"de.aikiit.mailversendala.template"}];updateSearchResults();
