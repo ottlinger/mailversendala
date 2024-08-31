a76670ba16999717e01218d36f1f1c61ff902cba
Hugo Hirsch <github@hugo-hirsch.de>
2024-08-31 13:56:16 +0200

    Add javadoc

--- src/main/java/de/aikiit/mailversendala/template/MailTemplate.java
+++ src/main/java/de/aikiit/mailversendala/template/MailTemplate.java
@@ -4,10 +4,22 @@ package de.aikiit.mailversendala.template;
  * Contains the contents of a mail.
  */
 public interface MailTemplate {
+    /**
+     * Default template file name for HTML.
+     */
     String BASE_NAME_HTML = "mailtemplate.html";
+    /**
+     * Default template file name for plain text
+     */
     String BASE_NAME_PLAINTEXT = "mailtemplate.txt";
 
+    /**
+     * @return mailing content in format HTML.
+     */
     String getHtml();
 
+    /**
+     * @return mailing content in plain text format.
+     */
     String getPlaintext();
 }
