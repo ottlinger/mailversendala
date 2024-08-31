e4eb4b7d480c04e22bf37000efabf94b34aa87c6
Hugo Hirsch <github@hugo-hirsch.de>
2024-08-31 14:38:57 +0200

    Add javadoc

--- src/main/java/de/aikiit/mailversendala/MailingResult.java
+++ src/main/java/de/aikiit/mailversendala/MailingResult.java
@@ -3,11 +3,20 @@ package de.aikiit.mailversendala;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Class encapsulates internal counters to gauge
+ * <li>how many errors and</li>
+ * <li>how many mails were successfully sent out</li>
+ * during an application run.
+ */
 public class MailingResult {
 
     private AtomicInteger errorCounter;
     private AtomicInteger mailCounter;
 
+    /**
+     * Increment internal error counter by 1.
+     */
     public void addError() {
         if (errorCounter == null) {
             this.errorCounter = new AtomicInteger(0);
@@ -15,6 +24,9 @@ public class MailingResult {
         errorCounter.incrementAndGet();
     }
 
+    /**
+     * Increment internal success counter by 1.
+     */
     public void addSuccess() {
         if (mailCounter == null) {
             this.mailCounter = new AtomicInteger(0);
@@ -22,14 +34,23 @@ public class MailingResult {
         mailCounter.incrementAndGet();
     }
 
+    /**
+     * @return current error counter if existing.
+     */
     public Optional<AtomicInteger> getErrorCounter() {
         return Optional.ofNullable(errorCounter);
     }
 
+    /**
+     * @return current success counter if existing.
+     */
     public Optional<AtomicInteger> getMailCounter() {
         return Optional.ofNullable(mailCounter);
     }
 
+    /**
+     * @return sum of {@link #mailCounter} and {@link #errorCounter}.
+     */
     public int getTotal() {
         return getErrorCounter().orElse(new AtomicInteger(0)).get() + getMailCounter().orElse(new AtomicInteger(0)).get();
     }
