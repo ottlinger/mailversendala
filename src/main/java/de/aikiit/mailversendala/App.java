755c278c4b0b4a62581a6f7ffdc1418cdc3231a3
Hugo Hirsch <github@hugo-hirsch.de>
2024-08-31 13:49:11 +0200

    Add javadoc

--- src/main/java/de/aikiit/mailversendala/App.java
+++ src/main/java/de/aikiit/mailversendala/App.java
@@ -13,6 +13,11 @@ public class App {
     private static final Logger LOG =
             LogManager.getLogger(App.class);
 
+    /**
+     * Main method to launch MailVersendala with the given arguments.
+     * @param args parameters for mail sending.
+     * @throws IOException in case of errors.
+     */
     public static void main(String[] args) throws IOException {
         Mailversendala.sendOut(new MailConfig());
         LOG.info("Mailversendala is shutting down. Bye Bye :-)");
